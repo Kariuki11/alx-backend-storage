2076f653d2f8c17585ec106980f3047803b387f0
Kariuki11 <irungukenneth433@gmail.com>
2024-08-09 02:09:08 +0300

    New file

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,8 @@
+-- Rank country origins of the bands
+-- Ordered by the number of (non-unique) fans
+-- Column names must be: origin and nb_fans
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands 
+GROUP BY origin
+ORDER BY nb_fans DESC;
