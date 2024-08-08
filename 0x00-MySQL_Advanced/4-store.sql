dfd9923740f9f12147b9b3156bef6d8c34bad260
Kariuki11 <irungukenneth433@gmail.com>
2024-08-09 02:15:34 +0300

    New file

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,11 @@
--- a trigger that decreases the quantity of an item after adding a new order.
-DROP TABLE IF EXISTS items;
-DROP TABLE IF EXISTS orders;
+-- Create a trigger that decreases the quantity
+-- of an item after adding a new order
+-- New & OLD are MySQL extensions to triggers
+-- enable to access columns in the rows affected by a trigger
 
-CREATE TABLE IF NOT EXISTS items (
-    name VARCHAR(255) NOT NULL,
-    quantity int NOT NULL DEFAULT 10
-);
-
-CREATE TABLE IF NOT EXISTS orders (
-    item_name VARCHAR(255) NOT NULL,
-    number int NOT NULL
-);
-
-INSERT INTO items (name) VALUES ("apple"), ("pineapple"), ("pear");
+CREATE TRIGGER decrease_quantity
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
